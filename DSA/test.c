e2ede04a49896465e2ae7165441683e952969d00
Roshan Limbu <topazkayle@gmail.com>
2023-06-24 21:57:59 +0545

    updated

--- DSA/test.c
+++ DSA/test.c
@@ -1,13 +1,41 @@
 #include <stdio.h>
 #include <conio.h>
-#include <string.h>
+#define MAX 10
 #include <process.h>
-#define MAX 100
-int main(){
-  int i, j;
-  char str[100];
-  printf("Enter your expression:");
-  fgets(str, MAX, stdin);
-  printf("Enter the value of %c",str[i]);
-  return 0;
+typedef struct {
+  int num[MAX];
+  int tos; 
+} stack;
+int isEmpty(stack* sp){
+  if(sp->tos==-1){
+    return -1;
+  } else {
+    return  0;
+  }
+}
+int isFull(stack* sp){
+  if(sp->tos==MAX-1){
+    return 1;
+  } else {
+    return 0;
+  }
+}
+void push (stack * sp){
+  if(isFull(sp)){
+    printf("Stack is full");
+    exit(0);
+  } else {
+    // sp->tos++;
+    // sp->num[sp->tos] = n;
+    sp->num[++sp->tos]= n; // pushing after increasing the stack by 1
+  }
+}
+void pop(stack* sp){
+  if(isEmpty(sp)){
+    printf("Stack Underflow.\n");
+    return;
+  }
+  n= sp->num[sp->tos];
+  sp->tos--;
+  printf("%d was popped.", n);
 }
