e425d784e9bef75f7f41202551b4dd1cd22e0c2e
Roshan Limbu <topazkayle@gmail.com>
2023-07-04 23:31:07 +0545

    doubly linked list code

--- DSA/doublylinkedlist.c
+++ DSA/doublylinkedlist.c
@@ -43,10 +43,51 @@ void insertAtLast() {
   if (header == NULL) {
     header = ptrnew;
   } else {
-    for (ptrthis = header; ptrthis->next != NULL; ptrthis = ptrthis->next);
-    ptrthis->next=ptrnew;
-    ptrnew->prev=ptrthis;
+    for (ptrthis = header; ptrthis->next != NULL; ptrthis = ptrthis->next)
+      ;
+    ptrthis->next = ptrnew;
+    ptrnew->prev = ptrthis;
   }
   printf("\nNode inserted at the end.");
 }
-void 
+
+void insertAfter() {
+  struct node *ptrnew, *ptrthis;
+  int n, target;
+  printf("\nEnter a node after which you want to inset: ");
+  scanf_s("%d", &target);
+  for (ptrthis = header; ptrthis != NULL; ptrthis = ptrthis->next) {
+    if (ptrthis->data == target) {
+      // insert and return
+      printf("Enter a new number to be inserted: ");
+      scanf_s("%d", &n);
+      ptrnew = getnode(n);
+      ptrnew->next = ptrthis->next;
+      ptrnew->prev = ptrthis;
+      if (ptrthis->next != ptrnew)
+        (ptrthis->next)->prev = ptrnew;
+      ptrthis->next=ptrnew;
+      return;
+    }
+  }
+}
+void insertBefore() {
+  struct node *ptrnew, *ptrthis;
+  int n, target;
+  printf("\nEnter a node after which you want to inset: ");
+  scanf_s("%d", &target);
+  for (ptrthis = header; ptrthis != NULL; ptrthis = ptrthis->next) {
+    if (ptrthis->data == target) {
+      // insert and return
+      printf("Enter a new number to be inserted: ");
+      scanf_s("%d", &n);
+      ptrnew = getnode(n);
+      ptrnew->prev = ptrthis->prev;
+      ptrnew->next = ptrthis;
+      if (ptrthis->prev != ptrnew)
+        (ptrthis->prev)->next = ptrnew;
+      ptrthis->prev=ptrnew;
+      return;
+    }
+  }
+}
--- DSA/stack2.c
+++ DSA/stack2.c
@@ -1,92 +1,81 @@
-#include <stdio.h>
 #include <conio.h>
+#include <stdio.h>
 #include <string.h>
-#define MAX 10 
-#define TRUE 1 
-#define FALSE 0 
-#define NUlL 0 
+#define MAX 10
+#define TRUE 1
+#define FALSE 0
+#define NUlL 0
 #include <stdlib.h>
 
 typedef struct {
   char items[MAX];
   int tos;
 } stack;
-int IsEmpty (stack* sp){
-  if(sp->tos==1){
+int IsEmpty(stack *sp) {
+  if (sp->tos == 1) {
     return TRUE;
   } else {
-  return FALSE;
-  }
-}
-int IsFull(stack* sp){
-  if(sp->tos==MAX-1){
-    return TRUE;
-  } else{
     return FALSE;
   }
 }
-void pop(stack *sp){
-  if(IsEmpty(sp)){
+int IsFull(stack *sp) {
+  if (sp->tos == MAX - 1) {
+    return TRUE;
+  } else {
+    return FALSE;
+  }
+}
+void pop(stack *sp) {
+  if (IsEmpty(sp)) {
     printf("\nStack Underflow");
     exit(0);
   }
   sp->tos--;
 }
-void push(stack   *sp, char ch){
-  if (IsFull(sp)){
+void push(stack *sp, char ch) {
+  if (IsFull(sp)) {
     printf("Stack is full.");
     exit(0);
   }
-  sp->items[++sp->tos]=ch;// pushing after increasing the stack by 1
+  sp->items[++sp->tos] = ch; // pushing after increasing the stack by 1
 }
-char peek(stack* sp){
-  if(IsEmpty(sp)){
+char peek(stack *sp) {
+  if (IsEmpty(sp)) {
     return 0;
   } else {
     return sp->items[sp->tos];
   }
 }
-int  main(){
+int main() {
   stack s;
-  int i, valid =TRUE, ch;
+  int i, valid = TRUE, ch;
   char exp[MAX];
-  s.tos=-1; // empty stack
+  s.tos = -1; // empty stack
   printf("Enter your expression: ");
-  fgets(exp,MAX,stdin);
-  for(i=0; i<strlen(exp); i++){
-    if(exp[i]=='{'|| exp[i]=='['|| exp[i]=='('){ // if opener
-        push(&s,exp[i]);
-    } else if (exp[i]=='}'|| exp[i]==']'|| exp[i]==')') { // if closer
-      if(IsEmpty(&s)){
+  fgets(exp, MAX, stdin);
+  for (i = 0; i < strlen(exp); i++) {
+    if (exp[i] == '{' || exp[i] == '[' || exp[i] == '(') { // if opener
+      push(&s, exp[i]);
+    } else if (exp[i] == '}' || exp[i] == ']' || exp[i] == ')') { // if closer
+      if (IsEmpty(&s)) {
         valid == FALSE;
         break;
-      }
-      else {
-        ch=peek(&s);
-        if((ch=='[' && exp[i]!=']')|| (ch=='{' && exp[i]!='}') || (ch=='(' && exp[i]!=')')){
-          valid= FALSE;
+      } else {
+        ch = peek(&s);
+        if ((ch == '[' && exp[i] != ']') || (ch == '{' && exp[i] != '}') ||
+            (ch == '(' && exp[i] != ')')) {
+          valid = FALSE;
           break;
         }
       }
-    } 
+    }
   }
-  if(IsEmpty(&s)==FALSE){
-    valid =FALSE;
+  if (IsEmpty(&s) == FALSE) {
+    valid = FALSE;
   }
-  if(valid==TRUE){
+  if (valid == TRUE) {
     printf("\nExpression is invalid");
-  } else{
+  } else {
     printf("\n Expression is valid");
   }
 }
-
-
-
-
-
-
-
-
-
-
-
